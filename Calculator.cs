91c6534fc315213c207a663407d8fcfb2f8bbb7f
marcelo0410 <cs95401156@gmail.com>
2022-08-01 23:34:26 +1000

    first commit

---
+++ Calculator.cs
@@ -0,0 +1,22 @@
+class Calculator
+{
+    public static String add(int num1, int num2)
+    {
+        return num1 + " + " + num2 + " = " + (num1 + num2);
+    }
+
+    public static String sub(int num1, int num2)
+    {
+        return num1 + " - " + num2 + " = " + (num1 - num2);
+    }
+
+    public static String mult(int num1, int num2)
+    {
+        return num1 + " * " + num2 + " = " + (num1 * num2);
+    }
+
+    public static String div(int num1, int num2)
+    {
+        return num1 + " / " + num2 + " = " + (num1 / num2);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text.RegularExpressions;
+// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Regex
+            Regex regexNumber = new Regex(@"[+-]?\d+\.?\d*");
+            Regex regexOperator = new Regex(@"[-+*/]");
+
+            // number and operator
+            int num1 = 0;
+            int num2 = 0;
+            String o1;
+            
+
+
+            // First number
+            while(true)
+            {
+                Console.WriteLine("Enter first number: ");
+                String s1 = Console.ReadLine();
+                
+                Match match = regexNumber.Match(s1);
+                if(match.Success)
+                {
+                    num1 = Convert.ToInt32(s1);
+                    break;
+                } else
+                {
+                    Console.WriteLine("Please enter valid number!");
+                }
+            }
+
+            // Operator
+            while(true)
+            {
+                Console.WriteLine("Enter operator: (+-*/) ");
+                o1 = Console.ReadLine();
+                
+                Match match = regexOperator.Match(o1);
+                if(match.Success)
+                {
+                    break;
+                } else
+                {
+                    Console.WriteLine("Please enter valid operator!");
+                }
+            }
+
+            // Second number
+            while(true)
+            {
+                Console.WriteLine("Enter second number: ");
+                String s2 = Console.ReadLine();
+                
+                Match match = regexNumber.Match(s2);
+                if(match.Success)
+                {
+                    num2 = Convert.ToInt32(s2);
+                    break;
+                } else
+                {
+                    Console.WriteLine("Please enter valid number!");
+                }
+            }
+            
+
+            // Calculation
+            switch (o1)
+            {
+                case "+":
+                    Console.WriteLine(Calculator.add(num1, num2));
+                    break;
+                case "-":
+                    Console.WriteLine(Calculator.sub(num1, num2));
+                    break;
+                case "*":
+                    Console.WriteLine(Calculator.mult(num1, num2));
+                    break;
+                case "/":
+                    Console.WriteLine(Calculator.div(num1, num2));
+                    break;        
+                default:
+                    Console.WriteLine("Error");
+                    break;  
+            }
+
+        }
+    }
+}
